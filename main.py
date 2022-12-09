a5e4374a4a051cc18f52b0876856cc0737627dc2
Samuel Haile <samhaile129@gmail.com>
2022-12-08 21:10:46 -0500

    Updated window title

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
-screen.title('Sam Snake Game')
+screen.title("Sam's Snake Game")
 screen.tracer(0)
 
 snake = Snake()
